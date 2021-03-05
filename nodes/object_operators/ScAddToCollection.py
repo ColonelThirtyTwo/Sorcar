867c6447808771e93cfb6206f488ce9c2d936f51
Alex Parrill <alex.parrill@col32.net>
2021-03-05 17:10:33 -0500

    Add "Add to Collection" node
    Allows you to add an object to a collection via nodes.

---
+++ nodes/object_operators/ScAddToCollection.py
@@ -0,0 +1,25 @@
+import bpy
+
+from bpy.props import StringProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+from .._base.node_operator import ScObjectOperatorNode
+
+class ScAddToCollection(Node, ScObjectOperatorNode):
+    bl_idname = "ScAddToCollection"
+    bl_label = "Add Object to Collection"
+
+    in_name: StringProperty(default="", update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketString", "Name").init("in_name", True)
+    
+    def error_condition(self):
+        return(
+            super().error_condition()
+            or self.inputs["Name"].default_value == ""
+        )
+
+    def functionality(self):
+        bpy.ops.collection.objects_add_active(collection=self.inputs["Name"].default_value)
