dd75baa68bb086e3f1ad0145c4c9d2c39df0d517
Alex Parrill <alex.parrill@col32.net>
2021-03-16 22:52:10 -0400

    Add Remove From [All] Collection[s] nodes

---
+++ nodes/object_operators/ScRemoveFromAllCollections.py
@@ -0,0 +1,18 @@
+import bpy
+
+from bpy.props import StringProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+from .._base.node_operator import ScObjectOperatorNode
+
+class ScRemoveFromAllCollections(Node, ScObjectOperatorNode):
+    bl_idname = "ScRemoveFromAllCollections"
+    bl_label = "Remove Object from All Collection"
+
+    def init(self, context):
+        super().init(context)
+
+    def functionality(self):
+        obj = self.inputs["Object"].default_value
+        for collection in obj.users_collection:
+            collection.objects.unlink(obj)
---
+++ nodes/object_operators/ScRemoveFromCollection.py
@@ -0,0 +1,29 @@
+import bpy
+
+from bpy.props import StringProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+from .._base.node_operator import ScObjectOperatorNode
+
+class ScRemoveFromCollection(Node, ScObjectOperatorNode):
+    bl_idname = "ScRemoveFromCollection"
+    bl_label = "Remove Object from Collection"
+
+    in_name: StringProperty(default="", update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketString", "Name").init("in_name", True)
+    
+    def error_condition(self):
+        return(
+            super().error_condition()
+            or self.inputs["Name"].default_value == ""
+            or self.inputs["Name"].default_value not in bpy.data.collections
+        )
+
+    def functionality(self):
+        collection = bpy.data.collections[self.inputs["Name"].default_value]
+        obj = self.inputs["Object"].default_value
+        if obj.name in collection.objects:
+            collection.objects.unlink(obj)
